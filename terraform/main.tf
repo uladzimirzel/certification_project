5f8f1ab7220fca03ad586b37d143a849574cf70d
uladzimirzel <test123@gmail.com>
2024-01-30 16:59:25 +0300

    UP main.tf and Jenkinsfile

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("peppy-web-405812-b1fdf9e085df.json")
+  credentials = file("/var/lib/jenkins/peppy-web-405812-49c67ecba27f.json")
   project     = "peppy-web-405812"
   region      = "europe-central2-a"
 }
@@ -21,6 +21,8 @@ resource "google_compute_instance" "build_instance" {
   boot_disk {
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-2004-lts"
+      size = "25"
+      type = "pd-ssd"
     }
   }
 
@@ -33,11 +35,13 @@ resource "google_compute_instance" "build_instance" {
 
   metadata = {
     ssh-keys = <<EOF
-    root: ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC5jlAhoTwcXJQNHZl3e0z/5jxiHdBgMk8hB9lYK0DcKAB4IEOIm7AUviYosnoG8VjkheyFWINH1MQdyYV/Mt/Zj1vxEpNpL2k5sViildscNNF9sdTraqPe76PyChUdN1ZAuoakIDzHdU5flST+QYbvYrWEplsx6bPwcnV/hLY/sKKhn+L6IYiXDnJ/NzRRNFR09kQ/J535v1MOwZ6yp0GwyYPFqofcCvNnDxGeaqo4eyvfP2DBLTMx4elAiL1ja9b8oXhrMGsxcM9MDL+b7xiX/m7DuCn8tWCVv+XImJ6QYmNFxqqaAPzy2BpQIB63DPUxN91UW/7mH61UaTk/X1ySG+dFhHGWFLnZVSeA4xNjupD+kS3utBlSVSxuoj7ImngV6JQv9Shj5hpp8WaTj866tDMfWfXLwjSy3yxwGXfyF3QmFwlx6KE+JFkaoH29gArjuZC3zyT4C/oV+wqm3R5Dmtzh6G5RafVdqDrn9576b9Qt940jOgrA6HZ8KPnKeZ8= root@test-terraform
+    root: ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDfPznW66aLU39FXRzzAZQsZeCv0Fni3CY/LAijT7gbzieTPZBk+TOuLhNsU34Sv/6AfmDeSTzIxNGlyoGh6rELz2AOkNxTCZq3raTyzbt1i6085i3grx8lUVScWyjai+wHW/b0cLYEypAihNi16K73yOGmQPKxKWjEZ7CDlo4TkgXkjvkC9KWjQPSEboaupP/DO9yO9ZJ+SQGQmzoKFrMb7+B15LQ8d8E2H0H1t7XpM8lo6h6Npfboe/VSg5lHMC4jP9C3uRdRGEL1BO3NhB8bGDKFKHUGpKukrAasZpmZWQGzttu432E+KY1B3LUr2BNLGpe5QWaIejpE9hsw0emN5trY66TnNY6cvi0gJnA8yc7Mi5Pmjei0/u0br98dUSINsk1fmB3c1wa91TzGAnIZLr+UQF0FgzulbblSJ7jv4Tjoz91S2LR98Iof0a0/843RaPxJL7n2mppOojCRJnHeOsrZ+hId9M+/mgs3vgUMNAB6xRtHBZas9fO8aup1QI0= root@jenkins
     jenkins: ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDB4LY/pIZRp5eehRlMMISHV2szjwapbJGeyTB1ykp/BV9rC0u6QqB7xHVD+mzgPg2+9tPexEf7lCbOz6ONGwf4b8vZ1lXN0dH3ASJsjGmSpazLrOnowGig0m96Bs8hAl6ZreThU7YIgnNxaIHr6nNTCKegrtJ7fsj2+oqvYrXK4LBc/s6s08LC7FAT/JZfOD/PKtnF7ajZLpP2WAi3upKSZHbWAOpgTn6dhc33I5LBQ73lyobDVM0bPkZcFE/XzcRX4n/rfjYMFPwmkloQwh22ghqUAAl34z3Jl9hyQTrY8F9q02b28m6LNBKeacqg+m2pJMS+88vjfkxE0NW9Z8GfdTIflbDmwwiV2gVFGSV5T/yEd1Vzr8/uhTVJooybfjSUNgsgGfH4u/0aiLX+WbjhoTP8n3JsQXG5BIjsdQo7TLtPkHCtxiqpqrG0eVsX3JmFfmvvJE5Mny5O5tnjxAYSwIVeHBBrzh/hnKIRDyLXu8lget6AUrE2y4oDugfiQxs= jenkins@jenkins
         EOF
   }
 }
+
+
 
 resource "null_resource" "build_instance" {
     depends_on = [ google_compute_instance.build_instance ]
@@ -46,9 +50,11 @@ provisioner "remote-exec" {
     inline = [
       "apt update",
       "apt install default-jdk -y",
-      "apt install docker.io -y",
-      "apt install docker-compose -y",
-      "echo {'insecure-registries': ['34.116.192.152:8123']} > /etc/docker/daemon.json"
+      "apt install docker.io -y && sleep 15",
+      "curl -L \"https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)\" -o /usr/local/bin/docker-compose",
+      "chmod +x /usr/local/bin/docker-compose",
+      "echo '{\"insecure-registries\":[\"34.116.192.152:8123\"]}' > /etc/docker/daemon.json",
+      "systemctl restart docker"
     ]
 
     connection {
@@ -56,8 +62,8 @@ provisioner "remote-exec" {
       user        = "root"
       private_key = file("/root/.ssh/id_rsa")
       host        = google_compute_instance.build_instance.network_interface[0].access_config[0].nat_ip
-     }
-   }  
+    }
+  }
 }
 
 resource "google_compute_instance" "stage_instance" {
@@ -82,7 +88,7 @@ resource "google_compute_instance" "stage_instance" {
 
   metadata = {
     ssh-keys = <<EOF
-    root: ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC5jlAhoTwcXJQNHZl3e0z/5jxiHdBgMk8hB9lYK0DcKAB4IEOIm7AUviYosnoG8VjkheyFWINH1MQdyYV/Mt/Zj1vxEpNpL2k5sViildscNNF9sdTraqPe76PyChUdN1ZAuoakIDzHdU5flST+QYbvYrWEplsx6bPwcnV/hLY/sKKhn+L6IYiXDnJ/NzRRNFR09kQ/J535v1MOwZ6yp0GwyYPFqofcCvNnDxGeaqo4eyvfP2DBLTMx4elAiL1ja9b8oXhrMGsxcM9MDL+b7xiX/m7DuCn8tWCVv+XImJ6QYmNFxqqaAPzy2BpQIB63DPUxN91UW/7mH61UaTk/X1ySG+dFhHGWFLnZVSeA4xNjupD+kS3utBlSVSxuoj7ImngV6JQv9Shj5hpp8WaTj866tDMfWfXLwjSy3yxwGXfyF3QmFwlx6KE+JFkaoH29gArjuZC3zyT4C/oV+wqm3R5Dmtzh6G5RafVdqDrn9576b9Qt940jOgrA6HZ8KPnKeZ8= root@test-terraform
+    root: ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDfPznW66aLU39FXRzzAZQsZeCv0Fni3CY/LAijT7gbzieTPZBk+TOuLhNsU34Sv/6AfmDeSTzIxNGlyoGh6rELz2AOkNxTCZq3raTyzbt1i6085i3grx8lUVScWyjai+wHW/b0cLYEypAihNi16K73yOGmQPKxKWjEZ7CDlo4TkgXkjvkC9KWjQPSEboaupP/DO9yO9ZJ+SQGQmzoKFrMb7+B15LQ8d8E2H0H1t7XpM8lo6h6Npfboe/VSg5lHMC4jP9C3uRdRGEL1BO3NhB8bGDKFKHUGpKukrAasZpmZWQGzttu432E+KY1B3LUr2BNLGpe5QWaIejpE9hsw0emN5trY66TnNY6cvi0gJnA8yc7Mi5Pmjei0/u0br98dUSINsk1fmB3c1wa91TzGAnIZLr+UQF0FgzulbblSJ7jv4Tjoz91S2LR98Iof0a0/843RaPxJL7n2mppOojCRJnHeOsrZ+hId9M+/mgs3vgUMNAB6xRtHBZas9fO8aup1QI0= root@jenkins
     jenkins: ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDB4LY/pIZRp5eehRlMMISHV2szjwapbJGeyTB1ykp/BV9rC0u6QqB7xHVD+mzgPg2+9tPexEf7lCbOz6ONGwf4b8vZ1lXN0dH3ASJsjGmSpazLrOnowGig0m96Bs8hAl6ZreThU7YIgnNxaIHr6nNTCKegrtJ7fsj2+oqvYrXK4LBc/s6s08LC7FAT/JZfOD/PKtnF7ajZLpP2WAi3upKSZHbWAOpgTn6dhc33I5LBQ73lyobDVM0bPkZcFE/XzcRX4n/rfjYMFPwmkloQwh22ghqUAAl34z3Jl9hyQTrY8F9q02b28m6LNBKeacqg+m2pJMS+88vjfkxE0NW9Z8GfdTIflbDmwwiV2gVFGSV5T/yEd1Vzr8/uhTVJooybfjSUNgsgGfH4u/0aiLX+WbjhoTP8n3JsQXG5BIjsdQo7TLtPkHCtxiqpqrG0eVsX3JmFfmvvJE5Mny5O5tnjxAYSwIVeHBBrzh/hnKIRDyLXu8lget6AUrE2y4oDugfiQxs= jenkins@jenkins
         EOF
   }
@@ -95,9 +101,11 @@ resource "null_resource" "stage_instance" {
     inline = [
       "apt update",
       "apt install default-jdk -y",
-      "apt install docker.io -y",
-      "apt install docker-compose -y",
-      "echo {'insecure-registries': ['34.116.192.152:8123']} > /etc/docker/daemon.json"
+      "apt install docker.io -y && sleep 15",
+      "curl -L \"https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)\" -o /usr/local/bin/docker-compose",
+      "chmod +x /usr/local/bin/docker-compose",
+      "echo '{\"insecure-registries\":[\"34.116.192.152:8123\"]}' > /etc/docker/daemon.json",
+      "systemctl restart docker"
     ]
 
     connection {
