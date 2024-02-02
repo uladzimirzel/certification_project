b284e2c77e2a7fda3018c84a8af830830b4c8b54
uladzimirzel <test123@mail.ru>
2024-02-03 00:46:32 +0300

    commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -52,10 +52,8 @@ resource "null_resource" "build_instance" {
 provisioner "remote-exec" {
     inline = [
       "apt-get update",
-      "apt-get update",
-      "sleep 20",
+      "apt-get install docker.io -y",
       "apt-get install python3-docker -y",
-      "apt-get install docker.io -y && apt-get install python3-docker -y",
       "sudo yes | gcloud auth configure-docker europe-central2-docker.pkg.dev",
       "sudo docker login https://europe-central2-docker.pkg.dev/peppy-web-405812/my-docker"
     ]
