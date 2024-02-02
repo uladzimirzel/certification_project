d911ab9b1c847b7e067cab0b275f51592753f572
uladzimirzel <test123@mail.ru>
2024-02-03 00:36:44 +0300

    commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -105,9 +105,7 @@ resource "null_resource" "stage_instance" {
 
     provisioner "remote-exec" {
     inline = [
-      "apt-get update && apt-get install docker.io -y",
-      "sudo yes | gcloud auth configure-docker europe-central2-docker.pkg.dev",
-      "sudo docker login https://europe-central2-docker.pkg.dev/peppy-web-405812/my-docker"
+      "apt-get update"
     ]
 
     connection {
